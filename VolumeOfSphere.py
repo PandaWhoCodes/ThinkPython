aebfcf0ff8e97c8c0ef69075e3b63813aed8fde6
ashsih101 <ufoundashish@gmail.com>
2015-10-18 21:51:45 +0530

    Update VolumeOfSphere.py

--- VolumeOfSphere.py
+++ VolumeOfSphere.py
@@ -2,9 +2,10 @@
 What is the volume of a sphere with the radius 5
 Hint: 392.7 is wrong
 """
-#For this we will be using the math.pi function in python
-#first we need to import the math class
+# For this we will be using the math.pi function in python
+# first we need to import the math class
 import math
-r=5
-volume=(4/3)*math.pi*r**3
+
+r = 5
+volume = (4 / 3) * math.pi * r ** 3
 print(volume)
