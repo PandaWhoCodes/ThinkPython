43afd68e3a2b4f5f6a6b3361f59bdc7229e93993
ashsih101 <ufoundashish@gmail.com>
2015-10-18 21:49:49 +0530

    Update DiscountCalc.py

--- DiscountCalc.py
+++ DiscountCalc.py
@@ -2,10 +2,10 @@
 for the first copy and 75 cents for each additional copy. What is the total wholesale cost of 60 copies
 """
 
-coverPrice=24.95
-discount=0.4*coverPrice
-disCountedPrice=coverPrice-discount
-#Shipping costs of first is 3$ remaining  59 copies=59*(75/100)
-Scost=3+(59*(0.75))
-totalPrice=(coverPrice*60)+Scost
+coverPrice = 24.95
+discount = 0.4 * coverPrice
+disCountedPrice = coverPrice - discount
+# Shipping costs of first is 3$ remaining  59 copies=59*(75/100)
+Scost = 3 + (59 * (0.75))
+totalPrice = (coverPrice * 60) + Scost
 print(totalPrice)
