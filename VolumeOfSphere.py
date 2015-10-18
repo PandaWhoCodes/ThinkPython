7f6e6bd0e038ea1ed9c5432f38117cef0a95b1be
ashsih101 <ufoundashish@gmail.com>
2015-10-18 18:21:56 +0530

    Create VolumeOfSphere.py

---
+++ VolumeOfSphere.py
@@ -0,0 +1,10 @@
+""" The volume of a sphere with radius r is (4/3)*pi*r^3
+What is the volume of a sphere with the radius 5
+Hint: 392.7 is wrong
+"""
+#For this we will be using the math.pi function in python
+#first we need to import the math class
+import math
+r=5
+volume=(4/3)*math.pi*r**3
+print(volume)
