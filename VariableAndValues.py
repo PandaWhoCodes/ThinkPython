e68058aa9dedf9ddf3142b2f8e68ace68357f857
ashsih101 <ufoundashish@gmail.com>
2015-10-18 21:50:51 +0530

    Update VariableAndValues.py

--- VariableAndValues.py
+++ VariableAndValues.py
@@ -11,31 +11,31 @@ for each of the expressions below write down the expression and the type
 4. 1+2*5
 5. delimiter*5
 """
-width= 17
-height=12.0
-delimiter='.'
+width = 17
+height = 12.0
+delimiter = '.'
 
-#1. Width /2 should give an integer value of 8.5-float
-first=width/2
-print (first)
+# 1. Width /2 should give an integer value of 8.5-float
+first = width / 2
+print(first)
 print(type(first))
 
-#Width /2.0 should give an integer value of 8.5-float
-second=width/2.0
+# Width /2.0 should give an integer value of 8.5-float
+second = width / 2.0
 print(second)
 print(type(second))
 
-#height/3 should give 4.0 as float
-third=height/3
+# height/3 should give 4.0 as float
+third = height / 3
 print(third)
 print(type(third))
 
 # Printing a simple mathematical expression will give an int value
-maths= 1+2*5
+maths = 1 + 2 * 5
 print(maths)
 print(type(maths))
 
-#delimiter*5 will print the delimiter five times and the type will be string
-print(delimiter*5)
-FiveDelimiter=delimiter*5
+# delimiter*5 will print the delimiter five times and the type will be string
+print(delimiter * 5)
+FiveDelimiter = delimiter * 5
 print(type(FiveDelimiter))
