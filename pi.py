ee0ebccc8e00c2e4839f9d732d39b6f59058909e
ashsih101 <ufoundashish@gmail.com>
2015-10-24 01:21:37 +0530

    chapter 7
    Added Question

--- pi.py
+++ pi.py
@@ -1,3 +1,10 @@
+"""The brilliant mathematician Srinivasa Ramanujan found an infinite series
+that can be used to generate a numerical approximation of pi:
+Write a function called estimate_pi that uses this formula to compute and
+return an estimate of pi. It should use a while loop to compute terms of
+the summation until the last term is smaller than 1e-15 (which is Python
+notation for 10^−15 ). You can check the result by comparing it to math.pi
+"""
 import math
 
 
