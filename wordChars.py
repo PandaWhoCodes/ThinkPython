b254e8a80985aee07e8a824c21d779fb54bca6ba
ashsih101 <ufoundashish@gmail.com>
2015-10-24 13:23:44 +0530

    Chapter 9
    Exercise 1

---
+++ wordChars.py
@@ -0,0 +1,10 @@
+"""
+Write a program that reads words.txt and prints only the words with more than 20
+characters
+"""
+with open('words.txt', 'r') as wd:
+    wordList = wd.read().split()
+
+for words in wordList:
+    if(len(words)>20):
+        print(words+" ")
