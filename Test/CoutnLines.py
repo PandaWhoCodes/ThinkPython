4d10b3f97c674151c6300610235c75288e20b06b
ashsih101 <ufoundashish@gmail.com>
2015-10-30 16:26:15 +0530

    CountLines
    From a given text

---
+++ Test/CoutnLines.py
@@ -0,0 +1,10 @@
+"""
+countlines:
+Read test.txt and count the lines and print the line count
+"""
+
+
+def countLines():
+    with open('test.txt') as text:
+        print(sum(1 for _ in text))
+countLines()
