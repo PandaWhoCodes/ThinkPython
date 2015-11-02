f4de852e7e0a6502459a159a65a7d2aeeb74c3f9
ashsih101 <ufoundashish@gmail.com>
2015-11-02 18:23:21 +0530

    The dictionary is having some problems

---
+++ Test/cleanWordFreq.py
@@ -0,0 +1,25 @@
+"""
+cleanwordfreq:S
+Repeat 4 but eliminate noise words
+(from noise.txt) from the word frequency table
+
+"""
+import re
+from collections import Counter
+
+noiseText = []
+with open('noise.txt') as noise:
+    for lines in noise:
+        noiseText = noiseText + lines.split()
+
+Text = []
+with open('test.txt') as text:
+    for lines in text:
+        Text = Text + re.findall(r"[\w']+", lines)
+cnt = Counter(Text)
+for word, count in cnt.items():
+    if word in noiseText:
+        if count > 4:
+            cnt[word] = 4
+for word, count in cnt.items():
+    print(word + ": " + str(count))
