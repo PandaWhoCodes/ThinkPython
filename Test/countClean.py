9ec6c85a3f4d3712dde96d037e0d0b8c873ec7e7
ashsih101 <ufoundashish@gmail.com>
2015-11-02 16:24:01 +0530

    Update countClean.py
    CountClean Working

--- Test/countClean.py
+++ Test/countClean.py
@@ -4,17 +4,22 @@ Read words from test.txt and 'noise words' from
 noise.txt. Print words from test.txt if it is not a noise word.
 
 """
+noiseText = []
+with open('noise.txt') as noise:
+    for lines in noise:
+        noiseText = noiseText + lines.split()
 
 
 def main():
-   
+    g = ""
     with open('test.txt') as text:
-        with open('noise.txt') as noiseText:
-            for line in text:
-                for word in noiseText:
-                    line = line.replace(word, "")
-                print(line)
+        for line in text:
+            line1 = line.split()
+            for words in line1:
+                if words not in noiseText:
+                    g = g + words + " "
+
+    print(g)
 
 
 main()
-#It Prints all the words
