69aecd28a5a0f10019c2dfb215000a84d211623f
ashsih101 <ufoundashish@gmail.com>
2015-11-02 17:50:09 +0530

    Fixed Split "" , ; error

--- Test/countFreq.py
+++ Test/countFreq.py
@@ -5,14 +5,13 @@ A word frequency table consists of a word and the number
 of times it occurs in the document
 
 """
-
+import re
 from collections import Counter
 
 Text = []
 with open('test.txt') as text:
     for lines in text:
-        Text = Text + lines.split()
+        Text = Text + re.findall(r"[\w']+", lines)
 cnt = Counter(Text)
 for word, count in cnt.items():
     print(word + ": " + str(count))
-
