e80126a6b76e455976403f5f2562e3fca5fcc06c
ashsih101 <ufoundashish@gmail.com>
2015-11-27 14:38:29 +0530

    Update time_increament.py

--- time_increament.py
+++ time_increament.py
@@ -9,6 +9,8 @@ In general, I recommend that you write pure functions whenever it is reasonable 
 to modifiers only if there is a compelling advantage. This approach might be called a
 functional programming style.
 """
+
+
 class Time(object):
     """
     This is our time object
@@ -16,40 +18,34 @@ class Time(object):
 
 
 def increment(seconds):
-
     if seconds > 60 and t.seconds < 60:
         temp = 60 - t.seconds
         seconds = seconds - temp
         t.seconds = 0
         t.minutes = t.minutes + 1
 
-    if t.minutes>60:
-        t.hour=t.hour+1
-        t.minutes=0
+    if t.minutes > 60:
+        t.hour = t.hour + 1
+        t.minutes = 0
 
+    if t.hour > 24:
+        t.hour = 0
 
-    if t.hour>24:
-        t.hour=0
-
-
-    if seconds <60:
-        t.seconds = t.seconds+ seconds
+    if seconds < 60:
+        t.seconds = t.seconds + seconds
         if t.seconds > 60:
-            t.seconds= t.seconds-60
+            t.seconds = t.seconds - 60
             t.minutes = t.minutes + 1
-            seconds =0
+            seconds = 0
 
-
-    if seconds !=0:
-
+    if seconds != 0:
         increment(seconds)
 
 
-
-t=Time()
-t.hour=12
-t.minutes=11
-t.seconds=45
+t = Time()
+t.hour = 12
+t.minutes = 11
+t.seconds = 45
 
 increment(195)
 print(str(t.hour) + ":" + str(t.minutes) + ":" + str(t.seconds))
