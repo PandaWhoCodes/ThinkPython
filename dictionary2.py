307f344a932ef608b1c04edaba9df1e326daac6f
Thomas Ashish Cherian <reach2ashish@users.noreply.github.com>
2016-01-18 21:43:08 +0530

    Update dictionary2.py

--- dictionary2.py
+++ dictionary2.py
@@ -5,10 +5,17 @@ Modify print_hist to print the keys and their values in alphabetical order.
 """
 
 hist = {1: 2, 3: 4, 5: 6, 7: 8}
+mydict = {'carl': 40,
+          'alan': 2,
+          'bob': 1,
+          'danny': 3}
 
 
 def print_hist():
-    for keys in hist:
-        print(keys)
+    for key in sorted(hist):
+        print(key)
+    for key in sorted(mydict):
+        print(key)
+
 
 print_hist()
