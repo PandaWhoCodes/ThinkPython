05d92de77acd5c65d8ecf2aeade060e4608a66aa
Thomas Ashish Cherian <reach2ashish@users.noreply.github.com>
2016-01-18 22:25:59 +0530

    Used nested list

--- lists1.py
+++ lists1.py
@@ -4,14 +4,17 @@ that takes a nested list of integers and add up
 the elements from all of the nested lists.
 """
 
-
-def nested_sum():
-    a = int(input("Enter the number of Elements you want to add:"))
-    mylist = []
-    for i in range(0, a):
-        mylist.append(int(input()))
-    mysum = sum(mylist)
-    print(mysum)
+mylist = [1, [73, 89, 42, 32], 62, [24, 32], 99]
 
 
-nested_sum()
+def nested_sum(mylist1):
+    total = 0
+    for i in mylist1:
+        if isinstance(i, list):  # checks if `i` is a list
+            total += nested_sum(i)
+        else:
+            total += i
+    return total
+
+
+print(nested_sum(mylist))
