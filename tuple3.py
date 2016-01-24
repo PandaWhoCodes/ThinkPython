873869897ba23ae7304d2432649939435cde5dd7
Thomas Ashish Cherian <reach2ashish@users.noreply.github.com>
2016-01-24 11:11:50 +0530

    Update tuple3.py

--- tuple3.py
+++ tuple3.py
@@ -2,6 +2,8 @@
 Write a function called most_frequent that takes a
 string and prints the letters
 in decreasing order of frequency.
+Only immutable elements can be used as dictionary keys, and hence only tuples and not lists can be used as keys.
+Hence distionary is a combination of Tuples and Lists
 """
 import operator
 import re
@@ -12,7 +14,7 @@ with open('test.txt') as text:
     for lines in text:
         Text = Text + re.findall(r"[\w']+", lines)
 cnt = Counter(Text)
-sorted_tuple = sorted(cnt.items(), key=operator.itemgetter(1), reverse=True)
-for word, count in sorted_tuple:
+sorted_dict = sorted(cnt.items(), key=operator.itemgetter(1), reverse=True)
+for word, count in sorted_dict:
     print(word + ": " + str(count))
 # print(sorted_tuple)
